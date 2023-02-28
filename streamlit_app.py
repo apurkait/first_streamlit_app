f52fe84ca9142ee7cb282bac9c41c9b9b733ecc5
apurkait <79083303+apurkait@users.noreply.github.com>
2023-02-28 13:14:04 +0530

    Update streamlit_app.py
    Importing snowflake package in Python code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Print Normalized Data
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
